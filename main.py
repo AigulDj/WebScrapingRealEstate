55e94ee75fed601cdb2901f7f4f9dcd76ec54aa8
Aigul <you@example.com>
2022-09-10 17:04:06 +0100

    Fix bug in page_num & date. Add 'recreate' func

--- main.py
+++ main.py
@@ -1,95 +1,95 @@
 import os
-# import time
 from datetime import datetime, timedelta
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-MAX_PAGE_NUM = 21
-TODAY = datetime.now().strftime("%d/%m/%Y")
-YESTERDAY = (datetime.now() - timedelta(1)).strftime("%d/%m/%Y")
+MAX_PAGE_NUM = 20
+TODAY = datetime.now().strftime("%d-%m-%Y")
+YESTERDAY = (datetime.now() - timedelta(1)).strftime("%d-%m-%Y")
 PASSWORD = os.getenv('PASSWORD')
 CHROME_DRIVER_PATH = "chromedriver"
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+
+# Scheme: "postgresql+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
 DATABASE_URI = f'postgresql+psycopg2://postgres:{PASSWORD}@localhost:5432/apartments'
 
 Base = declarative_base()
 engine = create_engine(DATABASE_URI)
-Session = sessionmaker(bind=engine)
-s = Session()
+Session = sessionmaker(bind=engine)  # To interact with the new table created
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+s = Session()
 
 
-# *************************** MODELS ***************************
+def recreate_database():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
 
 class Apartment(Base):
-    __tablename__ = "apartments"
+    __tablename__ = "test"
     id = Column(Integer(), primary_key=True)
-    image = Column(String())
-    title = Column(String(), nullable=False)
-    date = Column(String())
-    location = Column(String())
-    bedroom = Column(String())
-    description = Column(String())
-    price = Column(String())
-    currency = Column(String())
+    Image = Column(String())
+    Title = Column(String(), nullable=False)
+    Date = Column(String())
+    Location = Column(String())
+    Bedroom = Column(String())
+    Description = Column(String())
+    Price = Column(String())
+    Currency = Column(String())
 
     def __repr__(self):
-        return f"<Apartment(image='{self.image}', title='{self.title}', date='{self.date}', " \
-               f"location='{self.location}', bedroom='{self.bedroom}', description='{self.description}', " \
-               f"price='{self.price}', currency='{self.currency}')>"
+        return f"<Apartment(image='{self.Image}', title='{self.Title}', date='{self.Date}', " \
+               f"location='{self.Location}', bedroom='{self.Bedroom}', description='{self.Description}', " \
+               f"price='{self.Price}', currency='{self.Currency}')>"
 
 
-Base.metadata.create_all(engine)
-
-# *************************** CONFIG CRUD ***************************
+recreate_database()  # Change it to Base.metadata.create_all(engine) if no need to delete previous data in the table
 
 for num in range(1, MAX_PAGE_NUM + 1):
     page_num = f"page-{num}"
-    KIJIJI_URL = f"https://www.kijiji.ca/b-apartments-condos/city-of-toronto/{page_num}c37l1700273"
+    KIJIJI_URL = f"https://www.kijiji.ca/b-apartments-condos/city-of-toronto/{page_num}/c37l1700273"
     driver.get(KIJIJI_URL)
-    # time.sleep(10)
 
-# Pars & retrieve data, get selenium objects.
+    # Pars & retrieve data, get list of selenium objects.
     images = driver.find_elements(By.CSS_SELECTOR, ".image img")
     titles = driver.find_elements(By.CSS_SELECTOR, '.title a')
     dates = driver.find_elements(By.CLASS_NAME, "date-posted")
     locations = driver.find_elements(By.CSS_SELECTOR, ".location span:not(.date-posted)")
-    beds = driver.find_elements(By.CLASS_NAME, "bedrooms")
+    bedrooms = driver.find_elements(By.CLASS_NAME, "bedrooms")
     descriptions = driver.find_elements(By.CLASS_NAME, "description")
     prices = driver.find_elements(By.CLASS_NAME, "price")
     currencies = driver.find_elements(By.CLASS_NAME, "price")
 
-# Convert selenium objects into strings & return readable data.
-    image = [img.get_attribute('data-src') if True else "n/a" for img in images]
-    title = [title.text for title in titles]
-    date = [TODAY if "ago" in date.text else YESTERDAY if "Yesterday" in date.text else date.text for date in dates]
-    location = [loc.text for loc in locations]
-    bedroom = [bed.text for bed in beds]
-    description = [des.text for des in descriptions]
-    price = [price.text[1:] if price.text != "Please Contact" else "n/a" for price in prices]
-    currency = [cur.text[0] for cur in currencies]
+    # Convert selenium objects to strings & return readable data.
+    images = [img.get_attribute('data-src') if True else "n/a" for img in images]
+    titles = [title.text for title in titles]
+    dates = [TODAY if "ago" in date.text else YESTERDAY if "Yesterday" in date.text else date.text.replace('/', '-') for
+             date in dates]
+    locations = [loc.text for loc in locations]
+    bedrooms = [bed.text for bed in bedrooms]
+    descriptions = [des.text for des in descriptions]
+    prices = [price.text[1:] if price.text != "Please Contact" else "n/a" for price in prices]
+    currencies = [cur.text[0] for cur in currencies]
 
-    for i in range(len(title) - 1):
+    for i in range(len(titles)):
         apartment = Apartment(
-                image=image[i],
-                title=title[i],
-                date=date[i],
-                location=location[i],
-                bedroom=bedroom[i],
-                description=description[i],
-                price=price[i],
-                currency=currency[i]
+            Image=images[i],
+            Title=titles[i],
+            Date=dates[i],
+            Location=locations[i],
+            Bedroom=bedrooms[i],
+            Description=descriptions[i],
+            Price=prices[i],
+            Currency=currencies[i]
         )
         s.add(apartment)
         s.commit()
