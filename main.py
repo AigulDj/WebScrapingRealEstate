f20acaa94d167d07e80f497c658ed6ec41ec42a2
Your Name <you@example.com>
2022-09-15 18:20:35 +0100

    merged with main

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from gsheets import GoogleSheets
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,6 +27,10 @@ DATABASE_URI = f'postgresql+psycopg2://postgres:{PASSWORD}@localhost:5432/apartm
 Base = declarative_base()
 engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)  # To interact with the new table created
+<<<<<<< Updated upstream
+=======
+#s = Session()
+>>>>>>> Stashed changes
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 s = Session()
 
@@ -80,6 +86,7 @@ for num in range(1, MAX_PAGE_NUM + 1):
     prices = [price.text[1:] if price.text != "Please Contact" else "n/a" for price in prices]
     currencies = [cur.text[0] for cur in currencies]
 
+<<<<<<< Updated upstream
     for i in range(len(titles)):
         apartment = Apartment(
             Image=images[i],
@@ -94,5 +101,46 @@ for num in range(1, MAX_PAGE_NUM + 1):
         s.add(apartment)
         s.commit()
 s.close()
+=======
+    # for i in range(len(titles)):
+    #     apartment = Apartment(
+    #         Image=images[i],
+    #         Title=titles[i],
+    #         Date=dates[i],
+    #         Location=locations[i],
+    #         Bedroom=bedrooms[i],
+    #         Description=descriptions[i],
+    #         Price=prices[i],
+    #         Currency=currencies[i]
+    #     )
+    #     s.add(apartment)
+    # s.commit()
+
+    # ************** Upload data into Google Sheet **************
+    # SHEET_ENDPOINT = "https://api.sheety.co/38e04257acb33302247cfa32529cf529/apartments/add"
+    # try:
+    #     for i in range(len(titles)):
+    #         body = {
+    #             "add": {             # Don't forget to change 'add' to your sheet name
+    #                 "id": i + 1,
+    #                 "image": images[i],
+    #                 "title": titles[i],
+    #                 "date": dates[i],
+    #                 "location": locations[i],
+    #                 "bedroom": bedrooms[i],
+    #                 "description": descriptions[i],
+    #                 "price": prices[i],
+    #                 "currency": currencies[i]
+    #             }
+    #         }
+    #         sheet_response = requests.post(url=SHEET_ENDPOINT, json=body)
+    # except requests.exceptions.MissingSchema:
+    #     print('Invalid SHEET_ENDPOINT URL. Valid URL structure is https://api.sheety.co/username/projectName/sheetName')
+
+    #vals = (images, titles, dates, locations, bedrooms, descriptions, prices, currencies)
+    GoogleSheets.append_data(images, titles, dates, locations, bedrooms, descriptions, prices, currencies)
+
+#s.close()
+>>>>>>> Stashed changes
 
 driver.quit()
